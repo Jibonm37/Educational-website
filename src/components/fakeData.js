f7df0eda56c67d109fd5655e549b9d56946b1169
Jibonm37 <jibonrahman399@gmail.com>
2021-10-03 23:51:06 +0600

    first commit

---
+++ src/components/fakeData.js
@@ -0,0 +1,105 @@
+const fakeDate = [
+    {
+        id : 1,
+        topic : 'English 1st Paper',
+        videos : 120,
+        duration: '4hr 30m',
+        teacher:'Ayman Sadiq',
+        price:400,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShTFq4gHyzOO3Ofl_n27zPmFHep5jPTW7mYw&usqp=CAU'
+
+    },
+    {
+        id : 2,
+        topic : 'English Grammar',
+        videos : 80,
+        duration: '4hr 00m',
+        teacher:'Ayman Sofiq',
+        price:300,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNVc4mqY1JivIlw_n4PPdAdGDeoPsVwXZklw&usqp=CAU'
+
+    },
+    {
+        id : 3,
+        topic : 'Bangla 1st Paper',
+        videos : 200,
+        duration: '8hr 30m',
+        teacher:'Alamgir Hazari',
+        price:500,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS45d6rw6VT4XOC_EqXeomNWHb5M--6IX5uWw&usqp=CAU'
+
+    },
+    {
+        id : 4,
+        topic : 'Bangla 2nd Paper',
+        videos : 320,
+        duration: '4hr 30m',
+        teacher:'Mejbah',
+        price:600,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXJJeDXUtoy2ez_g_6z78SljVGpnrneZA7wQ&usqp=CAU'
+
+    },
+    {
+        id : 5,
+        topic : 'Accounting 1st Paper',
+        videos : 520,
+        duration: '10hr 30m',
+        teacher:'Abdullah Jibon',
+        price:1000,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRibN1t8kmnDzMegXa4OkiDJ3msUoYpgy7QZQ&usqp=CAU'
+
+    },
+    {
+        id : 6,
+        topic : 'Accounting 2nd Paper',
+        videos : 1100,
+        duration: '11hr 30m',
+        teacher:'Abdullah Jibon',
+        price:800,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvWokhYbH4dDRJuwjYQI9IFKaipbsaeMtmvg&usqp=CAU'
+
+    },
+    {
+        id : 7,
+        topic : 'Finance 1st Paper',
+        videos : 620,
+        duration: '4hr 30m',
+        teacher:'Abdullah',
+        price:500,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi_NVuRWTRcc6yWtizKVQGhR5Yh6GJJMl8Ig&usqp=CAU'
+
+    },
+    {
+        id : 8,
+        topic : 'ICT',
+        videos : 2000,
+        duration: '20hr 30m',
+        teacher:'Mohammed Iqbal',
+        price:1400,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTNuT7l1qqdVQm5EtjORPgup_Fv0CEZMe-9A&usqp=CAU'
+
+    },
+    {
+        id : 9,
+        topic : 'Economics 1st Paper',
+        videos : 620,
+        duration: '6hr 30m',
+        teacher:'Mossarof Tuhin',
+        price:700,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQobOaTJBecJhei5FzM_jyHiD6UPh4kvACe3w&usqp=CAU'
+
+    },
+    {
+        id : 10,
+        topic : 'Economics 2nd Paper',
+        videos : 320,
+        duration: '7hr 30m',
+        teacher:'Mossarof Tuhin',
+        price:600,
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjnxiEal1L7O4CSko1IJETBPvbtN7WDGwBig&usqp=CAU'
+
+    },
+    
+
+
+]
